b942fe1b18dbd6ac409b5c617d954e5684aee826
Dominik Liebl <liebl.dominik@gmail.com>
2019-03-21 14:42:05 +0100

    Update Make pkg

--- Make_package.R
+++ Make_package.R
@@ -1,12 +1,18 @@
 ## ################################
-
+##
+## Make Package Codes
+##
+## ################################
 
 ## Remove pkg 
 remove.packages("ffscb")
-## Create/update documentation
+
+## Create/update documentation and (re-)write NAMESPACE
 devtools::document("ffscb_pkg")      
+
 ## CRAN-check pkg
 # devtools::check("R/fregion_pkg")       # check the p-package
+
 ## Install
 devtools::install_local("ffscb_pkg")
 ## #################################
