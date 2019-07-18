045ba26fe24ec32aef9f1a676045f328a205baec
Dominik Liebl <liebl.dominik@gmail.com>
2019-07-18 16:35:48 +0200

    New non-stationary cov function

--- ffscb/R/covf.R
+++ ffscb/R/covf.R
@@ -15,12 +15,12 @@ covf.st.matern <- function(x1, x2, params = c(1,1)){
 
 #' Modified Matern Covariance Function (varying roughness parameter)
 #'
-#' @param x1 First argument of cov(x1,x2).
-#' @param x2 Second argument of cov(x1,x2).
+#' @param x1 First argument of cov(x1,x2). Caution: It is assumed that 0<=x1<=1.
+#' @param x2 Second argument of cov(x1,x2). Caution: It is assumed that 0<=x2<=1.
 #' @param params Covariance function parameters: params=c(nu1, nu2, sigma). 
 #' @export
 covf.nonst.matern <- function(x1,x2,params=c(3/2, 1/2, 1)){
-  nu    <- params[1] + min(x1,x2) * (params[2] - params[1])
+  nu    <- params[1] + max(x1,x2) * (params[2] - params[1])
   sigma <- params[3]  
   l     <- 1 
   d     <- sqrt(2*nu)*abs(x1-x2)/l
