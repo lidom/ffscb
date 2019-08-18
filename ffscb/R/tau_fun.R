67287d62ecdbc1ec4b0e73ae16576d8877976f0d
Dominik Liebl <liebl.dominik@gmail.com>
2019-08-18 13:32:22 +0200

    Robustify cov2tau function
    negative values from fragmentary fd are now linearly intepolated by the positve values

--- ffscb/R/tau_fun.R
+++ ffscb/R/tau_fun.R
@@ -133,26 +133,39 @@ tau_fragments <- function(X_mat, grid_mat){
 #' ylab="tau(t)", xlab="", ylim=range(tau_HR, tau_IR))
 #' par(mfrow=c(1,1))
 #' @export cov2tau_fun
-cov2tau_fun <- function(cov_mat){
+cov2tau_fun <- function(cov_mat, warn = FALSE){
   ## 'cov_mat' denotes the sample covariance function computed from the sample functions X_1(t),...X_n(t)
   ## Caution: assumed grid is in [0,1]
-  ##
   ##
   p        <- ncol(cov_mat)
   grid     <- seq(0,1,len=p)
   corr_mat <- stats::cov2cor(cov_mat) 
   ## computing the numeric approximation to c_12(t,s) := \partial^2 c(t,s)/(\partial t \partial t) 
   ## with c_12(t,s) evaluated at t=t and s=t
-  a1  <- corr_mat[cbind(2:p,2:p)]        # corr(t+h, t+h) with h=0.005, and t\in{0.005,0.015,0.025,...,0.995}
-  a2  <- corr_mat[cbind(1:(p-1),2:p)]    # corr(t-h, t+h) 
-  a3  <- corr_mat[cbind(1:(p-1),1:(p-1))]# corr(t-h, t-h) 
+  a1  <- 1 #Equals 1: corr_mat[cbind(2:p,2:p)]        # corr(t+h, t+h) with h=0.005, and t\in{0.005,0.015,0.025,...,0.995}
+  a2  <- corr_mat[cbind(1:(p-1),2:p)]                 # corr(t-h, t+h) 
+  a3  <- 1 #Equals 1: corr_mat[cbind(1:(p-1),1:(p-1))]# corr(t-h, t-h) 
   ##
   h   <- diff(grid)[1]/2
-  tau <- sqrt( c(a1- 2*a2 + a3) /( 4* h^2 ) )
+  suppressWarnings(
+    tau <- sqrt( c(a1 - 2*a2 + a3) /( 4* h^2 ) )
+  )
+  if(any(is.na(tau))){
+    if(warn){
+    warning("\nCaution there were negative tau-values!\n
+             Probably, this was caused by a cov-estimate\n
+             computed from fragmentary functions.\n
+             We use only the positive values and fill the \n
+             missing tau-values by approximations using linear interpolations.")
+    }
+    if(is.na(tau[1]  )){tau[1]   <- head(c(na.omit(tau)),n = 1)}
+    if(is.na(tau[p-1])){tau[p-1] <- tail(c(na.omit(tau)),n = 1)}
+    tau <- stats::approx(x=c(1:(p-1))[!is.na(tau)], y=tau[!is.na(tau)], xout = c(1:(p-1)))$y
+  }
   ## tau has length p-1, so we interpolate to get length p
   xx    <- c(0,      grid[-p] + h, 1               )
   yy    <- c(tau[1], tau,          tau[length(tau)])
-  tau_t <- stats::spline(y=yy, x=xx, xout = grid, method = "natural")$y
+  tau_t <- stats::approx(y=yy, x=xx, xout = grid)$y
   ##
   return(tau_t)
 }
