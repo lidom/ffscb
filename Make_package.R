d866af70d3722db048829b18e46d8ec562446923
dom <liebl.dominik@gmail.com>
2022-04-27 11:36:17 +0200

    Only adjusting a check-scenario

--- Make_package.R
+++ Make_package.R
@@ -24,13 +24,16 @@ help("ffscb")
 
 ## Generate a sample
 p          <- 200 
-N          <- 80 
+N          <- 15
 grid       <- make_grid(p, rangevals=c(0,1))
 mu0        <- meanf_poly(grid,c(0,0))   
 names(mu0) <- grid
 mu         <- meanf_poly(grid,c(0,0)) 
 names(mu)  <- grid
-cov.m      <- make_cov_m(cov.f = covf.nonst.matern, grid=grid, cov.f.params=c(2, 1/4, 1/4))
+# cov.m      <- make_cov_m(cov.f = covf.nonst.matern, grid=grid, cov.f.params=c(2, 1/4, 1/4))
+cov.m      <- make_cov_m(cov.f = covf.st.matern, grid=grid, cov.f.params=c(3/2, 1/4))
+
+
 sample     <- make_sample(mu,cov.m,N)
 
                                         # Compute the estimate and its covariance
@@ -41,21 +44,26 @@ hat.cov.mu <- hat.cov / N
                                         # Compute the tau-parameter (for the KR- and FFSCB-bands)
 hat.tau    <- tau_fun(sample)
 
-tau = hat.tau; diag.cov = diag(hat.cov.mu);  conf.level=0.95; n_int=4; df=N-1
+tau = hat.tau; diag.cov = diag(hat.cov.mu);  conf.level=0.95; n_int=3; df=N-1
 
 x = hat.mu; cov.x = hat.cov.mu; tau = hat.tau; df=N-1; conf.level  = 0.95; 
 
 
-FFSCB_t1 <- make_band_FFSCB_t(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, df = N-1, conf.level = 0.95, n_int = 1)
-FFSCB_t2 <- make_band_FFSCB_t(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, df = N-1, conf.level = 0.95, n_int = 8)
-FFSCB_z <- make_band_FFSCB_z(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, conf.level = 0.95)
+FFSCB_t1 <- make_band_FFSCB_t(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, df = N-1, conf.level = 0.95, n_int = 3)
+matplot(FFSCB_t1[,-1], type="l", lty=1, col=1)
+
+FFSCB_z1 <- make_band_FFSCB_z(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, conf.level = 0.95, n_int = 3)
+matplot(FFSCB_z1[,-1], type="l", lty=1, col=1)
 
 
-predict_band <- ffscb:::make_prediction_band_KR_t(x = hat.mu, diag.cov = diag(hat.cov.mu), tau = hat.tau, df=N-1, conf.level = 0.95)
+#FFSCB_t2 <- make_band_FFSCB_t(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, df = N-1, conf.level = 0.95, n_int = 8)
+#FFSCB_z <- make_band_FFSCB_z(x = hat.mu, diag.cov.x = diag(hat.cov.mu), tau = hat.tau, conf.level = 0.95)
 
 
-matplot(predict_band, type="l", lty=1, col=1)
+#predict_band <- ffscb:::make_prediction_band_KR_t(x = hat.mu, diag.cov = diag(hat.cov.mu), tau = hat.tau, df=N-1, conf.level = 0.95)
 
+
+#matplot(predict_band, type="l", lty=1, col=1)
 
 matplot(FFSCB_t1[,-1], type="l", lty=1, col=1)
 matlines(FFSCB_t2[,-1], type="l", lty=1, col=2)
