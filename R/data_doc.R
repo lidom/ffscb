2897cc8688162a0566a42f062f3d24d88f7e2cfb
dom <liebl.dominik@gmail.com>
2022-06-09 11:18:09 +0200

    complete revision of Readme.Rmd
    Including new setup of applications

--- R/data_doc.R
+++ R/data_doc.R
@@ -1,23 +1,23 @@
-#' Spinal bone mineral density measurements (spnbmd)
+#' Spinal bone mineral density measurements (Fragments)
 #'
-#' Relative spinal bone mineral density measurements on 261 North
+#' Relative spinal bone mineral density (spnbmd) measurements on 261 North
 #' American adolescents.  Each value is the difference in spnbmd
 #' taken on two consecutive visits, divided by the average. The age is
 #' the average age over the two visits.
 #'
 #' @format Variables:
 #' \describe{
-#'   \item{idnum}{identifies the child, and hence the repeat measurements}
-#'   \item{age}{average age of child when measurements were taken}
-#'   \item{gender}{male or female}
-#'   \item{spnbmd}{Relative Spinal bone mineral density measurement}
+#'   \item{Y_f.i}{spnbmd measurements of female subjects i=1,...,140}
+#'   \item{X_f.i}{ages of the female subjects at time of measurements}
+#'   \item{Y_m.i}{spnbmd measurements of male subjects i=1,...,113}
+#'   \item{X_m.i}{ages of the male subjects at time of measurements}
 #' }
 #' @source \url{http://web.stanford.edu/~hastie/ElemStatLearn/}
 #' @references 
 #' Bachrach LK, Hastie T, Wang M-C, Narasimhan B, Marcus R. Bone Mineral 
 #' Acquisition in Healthy Asian, Hispanic, Black and Caucasian Youth. A 
 #' Longitudinal Study. J Clin Endocrinol Metab (1999) 84, 4702-12.
-"spnbmd"
+"Fragments"
 
 
 #' Biomechanics Data (Biomechanics_Data)
